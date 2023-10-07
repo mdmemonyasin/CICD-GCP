bf64f9b7179d55a59dd3dc66ab4d3f1601b41326
Mohammad Yasin Memon <memonyasin3@gmail.com>
2023-10-07 14:24:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => {
-    return res.send('Hello World From Google IO Extended Durg!');
+    return res.send('Hello World! From 36 Inc');
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
