8070f5eccb56555a712880e2d69fcc4b8432b1f3
Mohammad Yasin Memon <memonyasin3@gmail.com>
2024-12-13 23:33:06 +0530

    updated routes

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => {
-    return res.send('Hello World! From Raipur!');
+// Root route
+app.get("/", (req, res) => {
+  return res.send("Hello World! From Raipur!");
+});
+
+// Route that works fine
+app.get("/success", (req, res) => {
+  return res.send("This route works perfectly!");
+});
+
+// Route that throws an error
+app.get("/error", (req, res) => {
+  throw new Error("This is an intentional error for logging purposes.");
+});
+
+// Route with a parameter that throws an error if the parameter is not a number
+app.get("/number/:num", (req, res) => {
+  const num = parseInt(req.params.num, 10);
+  if (isNaN(num)) {
+    throw new Error("The provided parameter is not a number.");
+  }
+  return res.send(`The number is ${num}`);
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
